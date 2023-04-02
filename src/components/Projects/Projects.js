7137d1f1ac1ec48ecc742eb938f591110ddce2cc
BaraaJadaan <braajad1@gmail.com>
2023-04-02 17:13:05 +0300

    updated

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import OneForAll from "../../Assets/Projects/OneForAll.png";
+import UpHills from "../../Assets/Projects/UpHills.png";
+import Stocking from "../../Assets/Projects/Stocking.png";
+import BrandingMind from "../../Assets/Projects/BrandingMind.png";
+import NFTMarket from "../../Assets/Projects/NFTMarket.png";
+import Footer from "../Footer";
+
+
+function Projects() {
+  return (
+    <div>
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          Recent <strong className="purple">Work </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few that I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={OneForAll}
+              title="OneForAll"
+              description="E-commerce website where people get to buy and sell products through a points-based billing system."
+              ghLink="https://github.com/BaraaJadaan/OneForAll"
+              demoLink="https://youtu.be/PU6Gmy2KrZI"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={UpHills}
+              title="UpHills"
+              description="Modern landing page for a bikes leasing company done using pure CSS"
+              ghLink="https://github.com/BaraaJadaan/UpHills"
+              demoLink="https://baraajadaan.github.io/UpHills/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Stocking}
+              title="Stocking"
+              description="Search for a stock and display its historical price changes and the latest finance news"
+              ghLink="https://github.com/BaraaJadaan/Stocking"
+              demoLink="https://baraajadaan.github.io/Stocking/"              
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={BrandingMind}
+              title="Branding Mind"
+              description="Modern website for a branding agency that showcase their portfolio"
+              ghLink="https://github.com/BaraaJadaan/brandingmind"
+              demoLink="https://baraajadaan.github.io/brandingmind/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={NFTMarket}
+              title="NFT Marketplace"
+              description="A website inspired by a Figma design for a company that makes acquiring NFTs to be as easy as possible"
+              ghLink="https://github.com/BaraaJadaan/nft-market"
+              demoLink="https://baraajadaan.github.io/nft-market/"
+            />
+          </Col>
+        </Row>
+
+      </Container>
+    </Container>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Projects;
---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,43 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import { AiOutlineDownload } from "react-icons/ai";
+import pdf from "../../Assets/Baraa Jadaan's Resume.pdf";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function ResumeNew() {
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", alignItems: "center"}}>
+          <Col
+            md={12}
+            style={{
+              position: "relative"
+            }}
+          >
+            <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px", marginBottom:"20px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+          </Col>
+          <Col
+            md={6}
+          >
+            <img style={{ width:'350px' }} src="https://i.ibb.co/8rQLd5d/Baraa-Jadaan.jpg" alt="Baraa-Jadaan" border="0"></img>
+          </Col>
+        </Row>
+      </Container>
+      </div>
+  );
+}
+
+export default ResumeNew;
